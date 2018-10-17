e2ff3658276d5a9107b35e225211f8d6d088d9b3
Nyrox <mark.oliver@hotmail.de>
2018-10-17 10:41:40 +0200

    Added ConciseCSS Config File

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,11 +1,14 @@
 
 import Vue from "vue";
+
+import App from "../templates/App.vue";
 import CIButton from '../templates/CIButton.vue';
 
 new Vue({
 	el: "#app",
-	template: "<CIButton />",
+	template: "<App />",
 	components: {
+		App,
 		CIButton
 	}
 })
